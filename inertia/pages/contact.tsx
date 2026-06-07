55d0279ef55efe2ce8c9e4e8e6f9d8a0451e4bd8
shellbaby <ediblechocolatething@gmail.com>
2026-06-07 18:41:04 -0400

    add: gallery and contact pages

--- inertia/pages/contact.tsx
+++ inertia/pages/contact.tsx
@@ -1,3 +1,63 @@
+import { Button, Field, Surface } from "~/components"
+
 export default function Page() {
-    return <>contact</>
+    return (
+        <>
+            <div className="grid grid-cols-2 gap-x-8">
+                <div>
+                    <Surface>
+                        <h5 className="mb-8 text-center">
+                            Contact me through here
+                        </h5>
+                        <div className="flex flex-col gap-4">
+                            <Button.Root color="var(--color-bluesky)" asChild>
+                                <a href="https://bsky.app/profile/poofy-eggnog.bsky.social">
+                                    Bluesky
+                                </a>
+                            </Button.Root>
+                            <Button.Root color="var(--color-telegram)" asChild>
+                                <a href="https://t.me/littlelambyderg">
+                                    Telegram
+                                </a>
+                            </Button.Root>
+                            <Button.Root color="var(--color-discord)" asChild>
+                                <a href="https://discord.com/users/845590906543407104">
+                                    Discord
+                                </a>
+                            </Button.Root>
+                        </div>
+                    </Surface>
+                </div>
+
+                <Surface>
+                    <h5 className="mb-8 text-center">
+                        ...or through this form
+                    </h5>
+                    <form className="flex flex-col gap-8">
+                        <Field.Root>
+                            <Field.Label>Name</Field.Label>
+                            <Field.Input />
+                        </Field.Root>
+
+                        <Field.Root>
+                            <Field.Label>Email</Field.Label>
+                            <Field.Input type="email" />
+                        </Field.Root>
+
+                        <Field.Root>
+                            <Field.Label>Subject</Field.Label>
+                            <Field.Input />
+                        </Field.Root>
+
+                        <Field.Root>
+                            <Field.Label>Message</Field.Label>
+                            <Field.Textarea autoresize />
+                        </Field.Root>
+
+                        <Button.Root>Submit</Button.Root>
+                    </form>
+                </Surface>
+            </div>
+        </>
+    )
 }
--- inertia/pages/gallery.tsx
+++ inertia/pages/gallery.tsx
@@ -1,3 +1,68 @@
 export default function Page() {
-    return <>gallery</>
+    const galleryImgs: { src: string; alt: string }[] = [
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_0.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_1.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_2.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_3.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_4.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_5.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_6.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_7.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_8.webp",
+            alt: "",
+        },
+
+        {
+            src: "https://daycare-cdn.qilin.cafe/static/images/gallery/2026/gallery_9.webp",
+            alt: "",
+        },
+    ]
+
+    return (
+        <>
+            <div
+                style={{
+                    columnCount: 4,
+                    columnGap: 0,
+                }}
+            >
+                {galleryImgs.map((img) => (
+                    <img src={img.src} alt={img.alt} className="p-1" />
+                ))}
+            </div>
+        </>
+    )
 }
